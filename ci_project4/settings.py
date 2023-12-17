1e1682bc0b83f229ca418ee50067f273354ed329
Yasmin Escaros-Buechsel <yescaros@apple.com>
2023-12-17 17:13:27 +0000

    FEAT: Adding custom user model

--- ci_project4/settings.py
+++ ci_project4/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 import os
 import dj_database_url
 
-development = os.environ.get('DEVELOPMENT', False)
+development = (os.environ.get
+               ('DEVELOPMENT', False))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -46,6 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'lego_collection'
 ]
+
+AUTH_USER_MODEL = 'lego_collection.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -129,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- lego_collection/models.py
+++ lego_collection/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+# Create your models here
+class CustomUser(AbstractUser):
+    user_id = models.IntegerField(unique=True)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.CharField(max_length=100, unique=True)
+    privacy = models.CharField(max_length=20, default='Private')
+    access = ArrayField(models.IntegerField(), blank=True, null=True)
+    collection_owner = models.IntegerField(blank=True, null=True)
+    account_type = models.CharField(max_length=20)
+    is_active = models.BooleanField(default=True)
+    REQUIRED_FIELDS = ['user_id','email','privacy','account_type']
