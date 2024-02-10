bacae7d7d87990ee95a45b7316ec0dcf8fa08eda
Yasmin Escaros-Buechsel <yescaros@apple.com>
2024-02-10 17:31:26 +0000

    FEAT(tests): adding form tests

---
+++ lego_collection/test_forms.py
@@ -0,0 +1,259 @@
+from django.test import TestCase
+from .forms import *
+
+
+class TestUpdateUsername(TestCase):
+    """
+    Testing form for updating usernames
+    """
+    def test_form_is_valid(self):
+        """
+        Check if overall change username form is valid
+        """
+        update_user_form = UpdateUsername({'username': 'test'})
+        self.assertTrue(update_user_form.is_valid(),
+                        msg='Failed: Valid username change form')
+
+    def test_form_invalid(self):
+        """
+        Check if overall change username form is invalid
+        """
+        update_user_form = UpdateUsername({'username': ''})
+        self.assertFalse(update_user_form.is_valid(),
+                         msg='Failed: Invalid username change form')
+
+
+class TestUpdatePrivacy(TestCase):
+    """
+    Testing form for updating privacy settings in account
+    """
+    def test_form_is_valid(self):
+        """
+        Check if overall change privacy form is valid
+        """
+        update_privacy_form = UpdatePrivacy(
+            {'privacy': 'PUB'})
+        self.assertTrue(update_privacy_form.is_valid(),
+                        msg='Failed: Valid privacy change form')
+
+    def test_form_invalid(self):
+        """
+        Check if overall change privacy form is invalid
+        """
+        update_privacy_form = UpdatePrivacy({'privacy': ''})
+        self.assertFalse(update_privacy_form.is_valid(),
+                         msg='Failed: Invalid privacy change form')
+
+    def test_form_invalid_choice(self):
+        """
+        Check if change privacy choice is invalid
+        """
+        update_privacy_form = UpdatePrivacy({'privacy': 'Public'})
+        self.assertFalse(update_privacy_form.is_valid(),
+                         msg='Failed: Invalid privacy change choice')
+
+
+class TestDeleteAccount(TestCase):
+    """
+    Testing form for deleting accounts
+    """
+    def test_form_is_valid(self):
+        """
+        Check if overall delete account form is valid
+        """
+        delete_acc_form = DeleteAccount(
+            {'delete': 1})
+        self.assertTrue(delete_acc_form.is_valid(),
+                        msg='Failed: Valid delete account form')
+
+    def test_form_invalid(self):
+        """
+        Check if overall delete account form is invalid
+        """
+        delete_acc_form = DeleteAccount(
+            {'delete': ''})
+        self.assertFalse(delete_acc_form.is_valid(),
+                         msg='Failed: Invalid delete account form')
+
+    def test_form_invalid_bool(self):
+        """
+        Check if delete account boolean is invalid
+        """
+        delete_acc_form = DeleteAccount(
+            {'delete': 0})
+        self.assertFalse(delete_acc_form.is_valid(),
+                         msg='Failed: Invalid delete account boolean')
+
+
+class TestCreateCollection(TestCase):
+    """
+    Testing form for creating collections
+    """
+    def test_form_is_valid(self):
+        """
+        Check if overall create collection form is valid
+        """
+        create_col_form = CreateCollection(
+            {'collection_name': 'test_collection'})
+        self.assertTrue(create_col_form.is_valid(),
+                        msg='Failed: Valid create collection form')
+
+    def test_form_invalid(self):
+        """
+        Check if overall create collection form is invalid
+        """
+        create_col_form = CreateCollection(
+            {'collection_name': ''})
+        self.assertFalse(create_col_form.is_valid(),
+                         msg='Failed: Invalid create collection form')
+
+
+class TestEditCollection(TestCase):
+    """
+    Testing form for editing collections
+    """
+    def test_form_is_valid(self):
+        """
+        Check if overall edit collection form is valid
+        """
+        edit_col_form = EditCollection(
+            {'collection_name': 'test_collection'})
+        self.assertTrue(edit_col_form.is_valid(),
+                        msg='Failed: Valid edit collection form')
+
+    def test_form_invalid(self):
+        """
+        Check if overall edit collection form is invalid
+        """
+        edit_col_form = EditCollection(
+            {'collection_name': ''})
+        self.assertFalse(edit_col_form.is_valid(),
+                         msg='Failed: Invalid edit collection form')
+
+
+class TestCreateSet(TestCase):
+    """
+    Testing form for creating sets
+    """
+    def test_form_is_valid(self):
+        """
+        Check if overall create set form is valid
+        """
+        create_set_form = CreateSet(
+            {'set_number': 12345, 'set_name': 'Test Set'})
+        self.assertTrue(create_set_form.is_valid(),
+                        msg='Failed: Valid create set form')
+
+    def test_form_invalid(self):
+        """
+        Check if overall create set form is invalid
+        """
+        create_set_form = CreateSet(
+            {'set_number': '', 'set_name': ''})
+        self.assertFalse(create_set_form.is_valid(),
+                         msg='Failed: Invalid create set form')
+
+    def test_form_invalid_nr(self):
+        """
+        Check if create set (set number) is invalid
+        """
+        create_set_form = CreateSet(
+            {'set_number': '', 'set_name': 'Test Set'})
+        self.assertFalse(create_set_form.is_valid(),
+                         msg='Failed: Invalid create set (set number) form')
+
+    def test_form_invalid_name(self):
+        """
+        Check if create set (set name) is invalid
+        """
+        create_set_form = CreateSet(
+            {'set_number': 12345, 'set_name': ''})
+        self.assertFalse(create_set_form.is_valid(),
+                         msg='Failed: Invalid create set (set name) form')
+
+
+class TestAddSet(TestCase):
+    """
+    Testing form for adding sets to collections
+    """
+    def setUp(self):
+        """
+        Create test set for test cases
+        """
+        LegoSet.objects.create(set_number=12345, set_name='Test Set')
+
+    def test_form_is_valid(self):
+        """
+        Check if overall add set form is valid
+        """
+        add_set_form = AddSet({'set': LegoSet.objects.get(set_number=12345),
+                               'build_status': 'NEW'})
+        self.assertTrue(add_set_form.is_valid(),
+                        msg='Failed: Valid add set form')
+
+    def test_form_invalid(self):
+        """
+        Check if overall add set form is invalid
+        """
+        add_set_form = AddSet({'set': '',
+                               'build_status': ''})
+        self.assertFalse(add_set_form.is_valid(),
+                         msg='Failed: Invalid add set form')
+
+    def test_form_invalid_set(self):
+        """
+        Check if add set (set) form is invalid
+        """
+        add_set_form = AddSet({'set': '',
+                               'build_status': 'STORED'})
+        self.assertFalse(add_set_form.is_valid(),
+                         msg='Failed: Invalid add set (set) form')
+
+    def test_form_invalid_status(self):
+        """
+        Check if add set (build status) form is invalid
+        """
+        add_set_form = AddSet({'set': LegoSet.objects.get(set_number=12345),
+                               'build_status': ''})
+        self.assertFalse(add_set_form.is_valid(),
+                         msg='Failed: Invalid add set (build status) form')
+
+    def test_form_invalid_choice(self):
+        """
+        Check if add set (build status choice) form is invalid
+        """
+        add_set_form = AddSet({'set': LegoSet.objects.get(set_number=12345),
+                               'build_status': 'Built'})
+        self.assertFalse(
+            add_set_form.is_valid(),
+            msg='Failed: Invalid add set (build status choice) form')
+
+
+class TestUpdateCol(TestCase):
+    """
+    Testing form for updating sets in collections
+    """
+    def test_form_is_valid(self):
+        """
+        Check if overall collection set update form is valid
+        """
+        update_col_form = UpdateCol({'build_status': 'NEW'})
+        self.assertTrue(update_col_form.is_valid(),
+                        msg='Failed: Valid update collection sets form')
+
+    def test_form_invalid(self):
+        """
+        Check if overall collection set update form is invalid
+        """
+        update_col_form = UpdateCol({'build_status': ''})
+        self.assertFalse(update_col_form.is_valid(),
+                         msg='Failed: Invalid update collection sets form')
+
+    def test_form_invalid_status(self):
+        """
+        Check if collection set update (build status choice) form is invalid
+        """
+        update_col_form = UpdateCol({'build_status': 'Built'})
+        self.assertFalse(
+            update_col_form.is_valid(),
+            msg='Failed: Invalid update sets (build status choice) form')
--- project4/settings.py
+++ project4/settings.py
@@ -10,10 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
-import os
 import cloudinary
 import dj_database_url
+import os
+import sys
+from pathlib import Path
 
 
 # Development variable
@@ -125,6 +126,8 @@ else:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
+if 'test' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
